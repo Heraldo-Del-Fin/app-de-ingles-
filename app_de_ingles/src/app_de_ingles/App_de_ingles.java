540d7289179147b37c0874eb9a81b0ca717685ec
Heraldo Del Fin <lserratoprincipe@correo.unicordoba.edu.co>
2022-07-03 22:38:54 -0500

    creacion del proyecto app de ingles

---
+++ app_de_ingles/src/app_de_ingles/App_de_ingles.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app_de_ingles;
+
+/**
+ *
+ * @author Hewlett Packard
+ */
+public class App_de_ingles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
